90f802ca79f37cd54eee5c3616035935de3d6974
eminebozdag <fyrs00@hotmail.com>
2023-01-02 19:22:13 +0100

    Add react-router

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      white: "#FFFFFF",
+      blue: "#119EE0",
+      yellow: "#F8F0CD",
+      "gray-light": "#F6F6F6",
+      gray: "#F2F3F5",
+      "font-gray": "#202125",
+      "footer-gray": "#141414",
+    },
+    fontFamily: {},
     extend: {},
   },
   plugins: [],
