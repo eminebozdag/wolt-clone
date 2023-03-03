49d35f2d4c550c2829fc44d4d3ad0ef3d5658bbe
eminebozdag <fyrs00@hotmail.com>
2023-03-03 18:01:15 +0100

    Add button tests

--- src/components/button.test.tsx
+++ src/components/button.test.tsx
@@ -1,4 +1,4 @@
-import {render} from "@testing-library/react";
+import {fireEvent, render} from "@testing-library/react";
 import Button, {Props} from "./button";
 
 describe("<Button/>", () => {
@@ -26,5 +26,42 @@ describe("<Button/>", () => {
 		expect(container.firstElementChild).toHaveClass(
 			"justify-center text-sm text-c-white font-semibold bg-c-blue px-[1rem] min-h-[2.875rem] rounded-lg",
 		);
+	});
+
+	it("should hover when hover is true", () => {
+		//Arrange
+		const props: Props = {variant: "primary", hover: true};
+
+		//Act
+		const {container} = render(<Button {...props} />);
+
+		//Assert
+		// eslint-disable-next-line testing-library/no-node-access
+		expect(container.firstElementChild).toHaveClass("hover:bg-c-blue/90");
+	});
+
+	it("should have children when children exist", () => {
+		//Arrange
+		const props: Props = {children: <div></div>};
+
+		//Act
+		const {container} = render(<Button {...props} />);
+		//Assert
+		// eslint-disable-next-line testing-library/no-node-access
+		expect(container.firstElementChild?.children.length).not.toBe(0);
+	});
+
+	it("should click event can properly", () => {
+		//Arrange
+		const props: Props = {};
+		const mockOnClick = jest.fn();
+
+		//Act
+		const {getByTestId} = render(<Button onClick={mockOnClick} {...props} />);
+		// eslint-disable-next-line testing-library/prefer-screen-queries
+		fireEvent.click(getByTestId("button-component"));
+
+		//Assert
+		expect(mockOnClick).toHaveBeenCalledTimes(1);
 	});
 });
