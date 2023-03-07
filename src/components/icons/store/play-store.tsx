706d22ab85ca96517b89079aa9ef776ef7394730
eminebozdag <fyrs00@hotmail.com>
2023-03-07 22:11:50 +0100

    Add footer test

--- src/components/icons/store/play-store.tsx
+++ src/components/icons/store/play-store.tsx
@@ -9,7 +9,8 @@ const GooglePlayStore = (props: Props) => {
 			href="https://play.google.com/store/apps/details?id=com.wolt.android&pid=google&c=DEU_Web_FTU_Search_Brand_Berlin_EN&af_adset=wolt&af_keywords=wolt&af_sub1=https://www.google.com/&af_sub2=/en/deu/berlin"
 			target="_blank"
 			rel="noreferrer"
-			className="inline-block rounded-2">
+			className="inline-block rounded-2"
+			data-testid="google-play-store-icon">
 			<img
 				aria-label="Download the Wolt Android app on Google Play"
 				src="https://consumer-static-assets.wolt.com/app-store-badges/google-play-badge_en.png"
