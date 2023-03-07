80b0775512b82cab305d637f08fa49f35d654893
eminebozdag <fyrs00@hotmail.com>
2023-03-07 12:29:46 +0100

    Fix header test

--- src/components/header/header.test.tsx
+++ src/components/header/header.test.tsx
@@ -1,16 +1,28 @@
 import {render, screen} from "@testing-library/react";
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import reducers from "../../store/combine";
 import Header from "./header";
 
-jest.mock("../container", () => jest.fn().mockReturnValue(<div data-testid="container-component"></div>));
+const mockStore = createStore(reducers, {
+	globalReducer: {},
+} as any);
 
 describe("<Header/>", () => {
 	it("should render successfully", () => {
 		// Arrange
+		const component = (
+			<Provider store={mockStore}>
+				<Header />
+			</Provider>
+		);
 
 		// Act
-		render(<Header />);
+		render(component);
 
 		// Assert
 		expect(screen.getByTestId("container-component")).not.toBeNull();
+		expect(screen.getByTestId("wolt-logo")).not.toBeNull();
+		expect(screen.getByTestId("login-component")).not.toBeNull();
 	});
 });
