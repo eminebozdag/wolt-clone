36ceabe46fb4b6f2a06fafe0080f87be791f6c21
eminebozdag <fyrs00@hotmail.com>
2023-03-08 19:52:17 +0100

    Add card test

--- src/pages/home/carousel/card.tsx
+++ src/pages/home/carousel/card.tsx
@@ -13,8 +13,9 @@ const Card = (data: Props) => {
 				"flex flex-1 flex-col max-w-[640px] text-center bg-c-section-gray",
 				"mx-4 mobile:mx-0 mobileM:mx-auto",
 				"my-[11px] mobile:my-4 desktop:my-0",
-			)}>
-			<img src={`/assets/card-images/${image}.jpg`} loading="lazy" alt="carousel_card" className="h-[300px] bg-center bg-cover" />
+			)}
+			data-testid="card-component">
+			<img src={`/assets/card-images/${image}.jpg`} loading="lazy" alt={`${image}`} className="h-[300px] bg-center bg-cover" />
 
 			<div className={clsx("flex flex-col flex-1 justify-between", "py-[30px] mobile:py-[37px]", "px-[25px] mobile:px-[39px]")}>
 				<h3 className={clsx("font-header font-[600] text-c-font-gray", "text-sm desktop:text-md", "pb-[14px] tabletL:pb-4")}>{title}</h3>
@@ -25,4 +26,5 @@ const Card = (data: Props) => {
 		</div>
 	);
 };
+export type {Props};
 export default Card;
