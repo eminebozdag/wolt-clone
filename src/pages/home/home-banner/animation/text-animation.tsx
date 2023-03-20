7d4c1a46bc8b5117c5a1fe8dd9bfda2044edbf8d
eminebozdag <fyrs00@hotmail.com>
2023-03-20 13:08:51 +0100

    Fix text animation

--- src/pages/home/home-banner/animation/text-animation.tsx
+++ src/pages/home/home-banner/animation/text-animation.tsx
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useMemo, useState} from "react";
 import ReactTextTransition from "react-text-transition";
 import TEXTS from "./text.config";
 const TextAnimation = () => {
@@ -7,27 +7,28 @@ const TextAnimation = () => {
 
 	useEffect(() => {
 		const intervalId = setInterval(() => setIndex((index) => index + 1), 6000);
-
-		return () => clearTimeout(intervalId);
+		return () => clearInterval(intervalId);
 	}, [index]);
 
-	return (
-		<div
-			className={clsx(
-				"relative font-header mb-[44px]",
-				"text-[3rem] tabletL:text-[4rem]",
-				"w-full",
-				"h-40 tablet:h-[16rem]",
-				"text-c-white mobile:text-c-font-gray",
-			)}
-			data-testid="text-animation-component">
-			<div className="absolute bottom-0 leading-[70px]">
-				{`${TEXTS[index % TEXTS.length]}`.split(" ").map((n, i) => (
-					<ReactTextTransition key={i} children={<span>{n}&nbsp;</span>} delay={i * 50} inline />
-				))}
+	return useMemo(() => {
+		return (
+			<div
+				className={clsx(
+					"relative font-header mb-[44px]",
+					"text-[3rem] tabletL:text-[4rem]",
+					"w-full",
+					"h-40 tablet:h-[16rem]",
+					"text-c-white mobile:text-c-font-gray",
+				)}
+				data-testid="text-animation-component">
+				<div className="absolute bottom-0 leading-[70px]">
+					{`${TEXTS[index % TEXTS.length]}`.split(" ").map((n, i) => (
+						<ReactTextTransition key={i} children={<span>{n}&nbsp;</span>} delay={i * 50} inline />
+					))}
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}, [index]);
 };
 
 export default TextAnimation;
